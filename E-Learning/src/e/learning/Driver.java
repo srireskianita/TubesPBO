5c879d54336fd906948bc887d9a1c015ac4996d9
LENOVO X230 <LENOVO X230@LENOVOX230-PC>
2016-03-26 21:45:30 +0700

    class Aplikasi & kelas Driver

---
+++ E-Learning/src/e/learning/Driver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.learning;
+
+/**
+ *
+ * @author LENOVO X230
+ */
+
+public class Driver {
+    public static void main(String[] args) {
+        Aplikasi apk= new Aplikasi();
+        apk.MainMenu();
+    }
+}
